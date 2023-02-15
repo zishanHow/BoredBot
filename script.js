b184d201544cb4f229e07911be04f0e7cda62448
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-02-16 01:26:56 +0600

    update boredapi link

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const activity = document.getElementById('activity')
 
 
 document.getElementById("btn").addEventListener("click", () => {
-    fetch('http://www.boredapi.com/api/activity/')
+    fetch('https://apis.scrimba.com/bored/api/activity')
     .then((response) => response.json())
     .then((data) => {
         activity.textContent = data.activity
