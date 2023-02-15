72b1ac49be5533f88f7fd672904f7b5eb80e050a
zishanHow <111298322+zishanHow@users.noreply.github.com>
2023-02-16 01:08:19 +0600

    added JS functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const activity = document.getElementById('activity')
+
+
+document.getElementById("btn").addEventListener("click", () => {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then((response) => response.json())
+    .then((data) => {
+        activity.textContent = data.activity
+        activity.classList.add("activity")
+
+        document.getElementById("titel").textContent = "Idea :-)"
+    })
+})
